0cafd1409fb8524aa89ae903b2fe2ba465dc897d
FrancescoGhinamo <44404163+FrancescoGhinamo@users.noreply.github.com>
2019-05-02 12:31:02 +0200

    Create ServerFileService.java

---
+++ src/fvc/backend/service/ServerFileService.java
@@ -0,0 +1,18 @@
+package fvc.backend.service;
+
+
+public class ServerFileService implements IFileService {
+
+	@Override
+	public void saveData(Object s, String dest) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object loadData(String source) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
